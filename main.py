ef7ff1ee619cb1e5a712df9ae7a90aa819598cd3
fanmo-yun <xiaomojun9527@outlook.com>
2024-06-28 15:51:12 +0800

    add get and post

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import argparse
 import flask
 from person import Person
-from flask import jsonify
+from flask import jsonify, request
 
 app = flask.Flask("axios-test")
 
@@ -29,15 +29,34 @@ def jsonv2():
 
 
 @app.get('/get/json/v3')
-def jsonv2():
+def jsonv3():
     data = [Person().jsonformat() for _ in range(0, 100)]
     return jsonify(data)
 
 
 @app.get('/get/json/v4')
-def jsonv2():
-    data = [Person().jsonformat() for _ in range(0, 100)]
-    return jsonify(data)
+def jsonv4():
+    num = request.args.get("num")
+    if num is None or num == "":
+        return jsonify({
+            "msg": "Miss num",
+        }), 400
+
+    if num.isnumeric() and int(num) > 0:
+        data = [Person().jsonformat() for _ in range(0, int(num))]
+        return jsonify(data)
+    else:
+        return jsonify({
+            "msg": "num is 不合法的",
+        }), 400
+
+
+@app.post("/post/v1")
+def postv1():
+    form_data = request.form.to_dict()
+    if not form_data:
+        return jsonify({'error': 'No form data found'}), 400
+    return jsonify(form_data), 200
 
 
 parser = argparse.ArgumentParser(description="An axios test app")
